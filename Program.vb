671b0568e76793b75f2a2d518f436f99aace5dab
Njay Garingarao <ninogaringarao13@gmail.com>
2024-10-15 02:30:16 +0800

    Chore (Program.vb) : Comments added

--- Program.vb
+++ Program.vb
@@ -4,13 +4,19 @@ Module Program
     Const LOWER_THRESHOLD As Integer = 0
     Private score As Integer = 0
 
+    ' Input Checker function that returns a boolean
     Function isValueValid(input As Integer) As Boolean
-
         Return (score + input <= UPPER_THRESHOLD) AndAlso (score + input >= LOWER_THRESHOLD)
     End Function
 
+
+    ' Implementation of the input checker
     Sub Main(args As String())
+
+        ' The main loop of the program
         While True
+
+            ' Initializes the consoles
             Console.Clear()
             Console.WriteLine($"--------------------------------------------------------------")
             Console.WriteLine($"| Current Score            : {score}")
@@ -18,6 +24,8 @@ Module Program
 
             Dim inputString As String = Console.ReadLine()
             Console.WriteLine($"| ")
+
+            ' Exits the program
             If inputString.ToLower() = "exit" Then
                 Console.WriteLine($"--------------------------------------------------------------")
                 Console.WriteLine("The program exited.")
@@ -25,11 +33,14 @@ Module Program
             End If
 
             Dim input As Integer
-            If Integer.TryParse(inputString, input) Then
-                If isValueValid(input) Then
-                    score += input
-                Else
 
+            ' Parses the input to integer, if failed, loop again.
+            If Integer.TryParse(inputString, input) Then
+
+                ' Checks if threshold is reached
+                If isValueValid(input) Then
+                    score += input ' Adds input into the score
+                Else
                     Console.WriteLine($"|!!!!!!!!!!!!!!!!!!!!!!!! ERROR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                     Console.WriteLine("| Inputted value and current score has reached the threshold!")
                     Console.WriteLine($"| Latest input rejected. Press any key to continue...")
